8a2962a85836aefbcc522c83354e20cc27e8d71d
Alex Mickelson <alexmickelson96@gmail.com>
2020-08-25 20:54:36 -0600

    got a websocket connection

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,48 @@
 import dotenv from "dotenv";
 import express from "express";
-
-dotenv.config();
-const port = process.env.SERVER_PORT;
-
 const app = express();
+import { log } from "./Services/LoggerService";
+import http from "http";
 
-app.get( "/", ( req, res ) => {
+import * as WebSocket from 'ws';
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+    log.info('a user connected');
+});
+
+
+// import cors from 'cors';
+// const options: cors.CorsOptions = {
+//     allowedHeaders: [
+//         'Origin',
+//         'X-Requested-With',
+//         'Content-Type',
+//         'Accept',
+//         'X-Access-Token',
+//     ],
+//     credentials: true,
+//     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+//     origin: "http://localhost:8080",
+//     preflightContinue: true,
+// };
+
+// app.use(cors(options));
+
+// dotenv.config();
+// const port = process.env.SERVER_PORT;
+
+
+app.get("/", (req, res) => {
     res.send("hello");
-} );
+});
 
-app.listen( port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+
+
+// app.listen(port, () => {
+//     log.info(`server started at http://localhost:${port}`);
+// });
+server.listen(3000, () => {
+    log.info(`Server started on port 3000)`);
+});
--- client/src/main.ts
+++ client/src/main.ts
@@ -5,6 +5,12 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// import VueSocketIOExt from 'vue-socket.io-extended';
+// import io from 'socket.io-client';
+ 
+// const socket = io('http://localhost:3000/');
+// Vue.use(VueSocketIOExt, socket);
+
 new Vue({
   router,
   store,
